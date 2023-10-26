526235f2e6ec47ad3a0932b6b736426638b4e86a
Saparin <SaparinOleg@gmail.com>
2023-10-26 10:34:57 +0300

    Add access control for user types

--- core/views.py
+++ core/views.py
@@ -1,10 +1,23 @@
 from django.contrib.auth import login
+from django.contrib.auth.mixins import AccessMixin, LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponseRedirect
+from django.views import View
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
-from django.urls import reverse_lazy, reverse
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse, reverse_lazy
 
 from core.forms import RegistrationForm
+
+
+class SuperuserRequiredMixin(UserPassesTestMixin, AccessMixin):
+    login_url = 'core:home'
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        return HttpResponseRedirect(reverse(self.login_url))
 
 
 class HomeView(TemplateView):
@@ -42,17 +55,20 @@ class ProductsListView(TemplateView):
     template_name = 'products_list.html'
 
 
-class ProductsNewView(TemplateView):
+class ProductsNewView(SuperuserRequiredMixin, TemplateView):
     template_name = 'products_new.html'
+    login_url = 'core:products'
 
 
-class ProductsEditView(TemplateView):
+class ProductsEditView(SuperuserRequiredMixin, TemplateView):
     template_name = 'products_edit.html'
+    login_url = 'core:products'
 
 
-class PurchasesListView(TemplateView):
+class PurchasesListView(LoginRequiredMixin, TemplateView):
     template_name = 'purchases_list.html'
+    login_url = 'core:login'
 
 
-class RefundsListView(TemplateView):
+class RefundsListView(SuperuserRequiredMixin, TemplateView):
     template_name = 'refunds_list.html'
